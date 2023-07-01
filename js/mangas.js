3cd2759d5db34eccc8f1b4b31216558446719f13
martu288 <martinalopez2812@gmail.com>
2023-07-01 12:27:42 -0300

    agregado de mangas.js

---
+++ js/mangas.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      url: 'http://fireforce.pythonanywhere.com/mangas',
+      mangas: [],
+      error: false,
+      cargando: true,
+    };
+  },
+  created() {
+    this.fetchData(this.url);
+  },
+  methods: {
+    fetchData(url) {
+      // Consumo de la API
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          this.mangas = data;
+          this.cargando = false;
+        })
+        .catch(err => {
+          console.error(err);
+          this.error = true;
+        });
+    },
+  },
+});
